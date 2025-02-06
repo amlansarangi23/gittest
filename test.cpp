e3dadd69e23568e65689d7ca3eecf6dd9743156e
Amlan Sarangi <amlan.sarangi23@gmail.com>
2025-02-06 17:38:10 +0530

    New update from amlanbranch

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
 
-    cout<<"Hello from amlan";
+    cout << "Hello from amlan";
+    cout << "Amlan Branch has added this change";
     return 0;
 }
